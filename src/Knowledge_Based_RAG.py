69f030e5903e2a8faf6e5b6c66b990c768af09ff
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2024-03-16 04:33:58 +0530

    Update Knowledge_Based_RAG.py

--- src/Knowledge_Based_RAG.py
+++ src/Knowledge_Based_RAG.py
@@ -1,1 +1,64 @@
+from torch import cuda
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.document_loaders import WebBaseLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Chroma
+from langchain.embeddings import GPT4AllEmbeddings
+from langchain.embeddings import LlamaCppEmbeddings
+from langchain.llms import LlamaCpp
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+embed_model_id = 'sentence-transformers/all-MiniLM-L6-v2'
+
+device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
+
+embed_model = HuggingFaceEmbeddings(
+    model_name=embed_model_id,
+    model_kwargs={'device': device},
+    encode_kwargs={'device': device, 'batch_size': 32}
+)
+
+##loader = WebBaseLoader("https://lilianweng.github.io/posts/2023-06-23-agent/")
+loader = WebBaseLoader("https://www.quadratics.com/MLOPSimplified.html")
+
+data = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=0)
+all_splits = text_splitter.split_documents(data)
+
+vectorstore = Chroma.from_documents(documents=all_splits, embedding=embed_model)
+
+n_gpu_layers = 32  # Metal set to 1 is enough.
+n_batch = 512  # Should be between 1 and n_ctx, consider the amount of RAM of your Apple Silicon Chip.
+callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+#llama = LlamaCppEmbeddings(model_path="/data/llama.cpp/models/llama-2-7b-chat/ggml-model-q4_0.bin")
+llm = LlamaCpp(
+    model_path="/data/llama.cpp/models/llama-2-13b-chat.ggmlv3.q5_1.bin",
+    n_gpu_layers=n_gpu_layers,
+    n_batch=n_batch,
+    n_ctx=2048,
+    f16_kv=True,  # MUST set to True, otherwise you will run into problem after a couple of calls
+    callback_manager=callback_manager,
+    verbose=False,
+)
+
+from langchain.chains import RetrievalQA
+
+rag_pipeline = RetrievalQA.from_chain_type(
+    llm=llm, chain_type='stuff',
+    retriever=vectorstore.as_retriever()
+)
+
+
+rag_pipeline(query)
+'''leave query for later'''
+
+
+
+
+
+
+
 
