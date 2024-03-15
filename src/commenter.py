706204d1cc4fc7a0a02b32de648a3931549d4e74
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2024-03-15 22:02:44 +0530

    Update commenter.py

--- src/commenter.py
+++ src/commenter.py
@@ -1,7 +1,6 @@
 import glob
 import os
 import torch
-import re
 from huggingface_hub import hf_hub_download
 from llama_cpp import Llama
 
@@ -34,7 +33,7 @@ CODE DIFFERENCE:
 {diff}
 """
     
-response=lcpp_llm(prompt=prompt_template, max_tokens=1536, temperature=0.5, top_p=0.95, repeat_penalty=1.2, top_k=75, echo=False)
+response=lcpp_llm(prompt=prompt_template, max_tokens=1536, temperature=0.5, top_p=0.95, repeat_penalty=1.2, top_k=50, echo=False)
 response = response["choices"][0]["text"]
 
 # Write the comment to the output file
